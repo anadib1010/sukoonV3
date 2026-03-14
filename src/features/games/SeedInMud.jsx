2dafa4620966acdb0d964dfc9078e22965aa8c48
Anadi <anadib1010@gmail.com>
2026-03-14 23:38:06 +0530

    update seedin mud

--- src/features/games/SeedInMud.jsx
+++ src/features/games/SeedInMud.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { SeedInMud } from '../games/SeedInMud';
 
 export function SeedInMud({ setTab, T, lang }) {
   const isHindi = lang === "Hindi";
